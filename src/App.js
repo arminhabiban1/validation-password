2a17de21e4b261b04bd8569f390cd428b129f59a
arminhabiban1 <132064497+arminhabiban1@users.noreply.github.com>
2023-08-03 12:53:58 +1000

    new project of validation of password

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Icon from 'react-icons-kit';
+import { basic_eye } from 'react-icons-kit/linea/basic_eye';
+import { basic_eye_closed } from 'react-icons-kit/linea/basic_eye_closed'
 
 function App() {
+  const [type, setType] = useState('password')
+
+  //validation states
+  const [lowervalidated, setlowervalidated] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="wrapper">
+
+      <div className="box">
+        <div className='input-with-icon-div form-contorl'>
+          <input type={type} className='custom-inpute' onChange={(e) => handleChange(e.target.value)} />
+          {type === 'password' ? (
+            <span onClick={() => setType('text')} className='icon-span'>
+              <Icon icon={basic_eye_closed} />
+            </span >
+          ) : (
+            <span onClick={() => setType('password')} className='icon-span'>
+              <Icon icon={basic_eye} />
+            </span>
+          )}
+          <main className='tracker-box'>
+            <div>At Least one Lowercase Letter</div>
+            <div>At Least one Uppercase Letter</div>
+            <div>At Least one one number</div>
+            <div>At Least one special Character</div>
+            <div>At Least B Characters</div>
+          </main>
+
+        </div>
+
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+    </div >
   );
 }
 
